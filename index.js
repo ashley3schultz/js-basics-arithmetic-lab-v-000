d98209e6f2b6bbe8bf598075f36745b672718c2f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-24 02:16:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ const randomNumber = Math.random() * 10 + 1;
 
 const randomInteger = Math.floor(randomNumber);
 
-const randomUserID = 1000000000 + randomNumber;
+const randomUserID = Math.random() * 10) + 1000000000
